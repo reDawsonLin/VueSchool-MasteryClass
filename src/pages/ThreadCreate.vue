219a748012e3d0489ba42f1a3e475c7a670cdf1d
reDawson <redawson357@gmail.com>
2022-09-14 22:11:28 +0800

    ch6 41. finished

---
+++ src/pages/ThreadCreate.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  forum: Object,
+});
+
+const title = ref("");
+const text = ref("");
+const save = () => {
+  console.log("save");
+};
+</script>
+
+<template>
+  <div class="col-full push-top">
+    <h1>
+      Create new thread in <i>{{ forum.name }}</i>
+    </h1>
+
+    <form @submit.prevent="save">
+      <div class="form-group">
+        <label for="thread_title">Title:</label>
+        <input
+          v-model="title"
+          type="text"
+          id="thread_title"
+          class="form-input"
+          name="title"
+        />
+      </div>
+
+      <div class="form-group">
+        <label for="thread_content">Content:</label>
+        <textarea
+          v-model="text"
+          id="thread_content"
+          class="form-input"
+          name="content"
+          rows="8"
+          cols="140"
+        ></textarea>
+      </div>
+
+      <div class="btn-group">
+        <button class="btn btn-ghost">Cancel</button>
+        <button class="btn btn-blue" type="submit" name="Publish">
+          Publish
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
